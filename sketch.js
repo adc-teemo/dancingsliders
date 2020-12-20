8c079878e5a2c1dd18481574d116295f7b0e4d29
adc-teemo <adc-teemo@users.noreply.github.com>
2020-12-21 00:54:04 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,33 +5,31 @@ var angle = 0;
 function setup() {
   createCanvas(200,200);
   textSize(15);
-  
-  rSlider = createSlider(0, 255, 100);
-  rSlider.position(20, 20);
-  gSlider = createSlider(0, 255, 0);
-  gSlider.position(20, 50);
-  bSlider = createSlider(0, 255, 255);
-  bSlider.position(20, 80);
-  for (var i = 0; i<10; i++){
+ 
+  for (var i = 0; i<100; i++){
   slider[i] = createSlider(0,255,50);
-  }
+  
+
+ 
 }
+}
+
 function draw() {
   var chuga = 0;
-
-  
-  for (var i = 0; i<10; i++){
-  var x = map(sin(angle+chuga),-1,1,0,255);
-  slider[i].value(x);
-  chuga += 0.1;
+  for(var i =0; i<slider.length; i++){
+    var x=map(sin(angle+chuga),-1,1,0,255);
+    slider[i].value(x);
+    chuga +=1
   }
-  angle += 0.1;
+  angle+=0.1;
   
-  const r = rSlider.value();
-  const g = gSlider.value();
-  const b = bSlider.value();
+  const r = slider[0].value();
+  const g = slider[1].value();
+  const b = slider[2].value();
+  
   background(r, g, b);
-  text('red', rSlider.x * 2 + rSlider.width, 35);
-  text('green', gSlider.x * 2 + gSlider.width, 65);
-  text('blue', bSlider.x * 2 + bSlider.width, 95);
+  
+  text('red', slider[0].x * 2 + slider[0].width, 35);
+  text('green', slider[1].x * 2 + slider[1].width, 65);
+  text('blue', slider[2].x * 2 + slider[2].width, 95);
 }
